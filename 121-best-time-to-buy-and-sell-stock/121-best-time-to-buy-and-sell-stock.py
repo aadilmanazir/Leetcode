80d448d2d130da29ef7db920842c9ab81cc88f63
aadilmanazir <71938309+aadilmanazir@users.noreply.github.com>
2022-10-08 00:19:52 -0700

    Time: 3961 ms (5.00%), Space: 25.8 MB (7.00%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,19 +1,16 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        #Kadane's algorithm
-        if len(prices) <= 1:
-            return 0
-        diff = [prices[i + 1] - prices[i] for i in range(len(prices) - 1)]
         curr_max = float('-inf')
-        curr = 0
-        for num in diff:
-            curr += num
-            if curr < 0:
-                curr = 0
-            curr_max = max(curr, curr_max)
-        return curr_max
-            
-        
+        l = []
+        for i in range(len(prices) - 1):
+            l.append(prices[i + 1] - prices[i])
+        curr_sum = 0
+        for x in l:
+            curr_sum += x
+            curr_max = max(curr_sum, curr_max)
+            curr_sum = max(curr_sum, 0)
+        return max(curr_max, 0)
+
         
             
         
