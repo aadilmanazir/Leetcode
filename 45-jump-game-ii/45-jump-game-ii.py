a38e04ac577cd96f9ba32663c5ae4baf04fe0500
aadilmanazir <71938309+aadilmanazir@users.noreply.github.com>
2022-10-10 14:15:05 -0700

    Time: 14196 ms (7.54%), Space: 15 MB (56.78%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -0,0 +1,26 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [float('inf')] * n
+        dp[-1] = 0
+        for i in range(n-2, -1, -1):
+            j = nums[i]
+            if j <= 0:
+                continue
+
+            curr_min = float('inf')
+            for k in range(i + 1, min(i + j + 1, n)):
+                curr_min = min(curr_min, dp[k] + 1)
+            dp[i] = curr_min
+        return dp[0]
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
